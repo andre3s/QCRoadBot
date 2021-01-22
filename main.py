6575c105591e299bbcde6cb394597df1455f3489
Andre Silveira <andre.silveira@oneocean.com>
2021-01-22 16:26:16 -0500

    - Finished weather forecast.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from extractor.road_status_extractor import road_status
+from extractor.weather_forecast import weather_forecast
 
 
 def main():
     """Execute main functions of this project."""
+    weather_forecast()
     road_status()
 
 
